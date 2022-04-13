d404c1a189aea2a46072fcf548e67e779f4a4acd
jinseong.cho <jinseong.cho@linecorp.com>
2022-04-13 18:28:11 +0900

    fix: fix multisignature spec file  error
    Multisignature is still encoded through Amino and prefix is added when encoded to Amino pubkey, but the prefix was not modified for ostracon, so multisig pubkey was not encoded correctly.
    Accordingly multisig address was not generated correctly.
    
    The problem was solved by modifying the prefix to match the ostracon.

--- packages/amino/src/encoding.ts
+++ packages/amino/src/encoding.ts
@@ -25,11 +25,11 @@ export function encodeSecp256k1Pubkey(pubkey: Uint8Array): Secp256k1Pubkey {
 // As discussed in https://github.com/binance-chain/javascript-sdk/issues/163
 // Prefixes listed here: https://github.com/tendermint/tendermint/blob/d419fffe18531317c28c29a292ad7d253f6cafdf/docs/spec/blockchain/encoding.md#public-key-cryptography
 // Last bytes is varint-encoded length prefix
-const pubkeyAminoPrefixSecp256k1 = fromHex("eb5ae987" + "21" /* fixed length */);
+const pubkeyAminoPrefixSecp256k1 = fromHex("c03701b7" + "21" /* fixed length */);
 const pubkeyAminoPrefixEd25519 = fromHex("1624de64" + "20" /* fixed length */);
 const pubkeyAminoPrefixSr25519 = fromHex("0dfb1005" + "20" /* fixed length */);
 /** See https://github.com/tendermint/tendermint/commit/38b401657e4ad7a7eeb3c30a3cbf512037df3740 */
-const pubkeyAminoPrefixMultisigThreshold = fromHex("22c1f7e2" /* variable length not included */);
+const pubkeyAminoPrefixMultisigThreshold = fromHex("77a72198" /* variable length not included */);
 
 /**
  * Decodes a pubkey in the Amino binary format to a type/value object.
--- packages/stargate/src/multisignature.spec.ts
+++ packages/stargate/src/multisignature.spec.ts
@@ -15,7 +15,6 @@ import { makeCompactBitArray, makeMultisignedTx } from "./multisignature";
 import { SignerData, SigningStargateClient } from "./signingstargateclient";
 import { assertIsDeliverTxSuccess, StargateClient } from "./stargateclient";
 import { faucet, pendingWithoutSimapp, simapp } from "./testutils.spec";
-import * as console from "console";
 
 describe("multisignature", () => {
   describe("makeCompactBitArray", () => {
@@ -171,118 +170,112 @@ describe("multisignature", () => {
   });
 
   // todo: should be check makeMultisignedTx unittest.
-  // describe("makeMultisignedTx", () => {
-  //   it("works", async () => {
-  //     pendingWithoutSimapp();
-  //     // const multisigAccountAddress = "cosmos1h90ml36rcu7yegwduzgzderj2jmq49hcpfclw9";
-  //     const multisigAccountAddress = "link164fvwlqv6actq6tvzqwmxjnu3ry7vr4rpskl8p";
-  //
-  //     // On the composer's machine signing instructions are created.
-  //     // The composer does not need to be one of the signers.
-  //     const signingInstruction = await (async () => {
-  //       const client = await StargateClient.connect(simapp.tendermintUrl);
-  //       const accountOnChain = await client.getAccount(multisigAccountAddress);
-  //       const latestBlockHeight = await client.getHeight();
-  //       assert(accountOnChain, "Account does not exist on chain");
-  //       console.log("latestBlockHeight:", latestBlockHeight);
-  //
-  //       const msgSend: MsgSend = {
-  //         fromAddress: multisigAccountAddress,
-  //         toAddress: "link1g7gsgktl9yjqatacswlwvns5yzy4u5jehsx2pz",
-  //         amount: coins(1234, "cony"),
-  //       };
-  //       const msg: MsgSendEncodeObject = {
-  //         typeUrl: "/lbm.bank.v1.MsgSend",
-  //         value: msgSend,
-  //       };
-  //       const gasLimit = 200000;
-  //       const fee = {
-  //         amount: coins(2000, "cony"),
-  //         gas: gasLimit.toString(),
-  //       };
-  //
-  //       return {
-  //         // accountNumber: accountOnChain.accountNumber,
-  //         sequence: accountOnChain.sequence,
-  //         chainId: await client.getChainId(),
-  //         msgs: [msg],
-  //         fee: fee,
-  //         sigBlockHeight: latestBlockHeight,
-  //         memo: "Use your tokens wisely",
-  //       };
-  //     })();
-  //
-  //     const [
-  //       [pubkey0, signature0, bodyBytes],
-  //       [pubkey1, signature1],
-  //       [pubkey2, signature2],
-  //       [pubkey3, signature3],
-  //       [pubkey4, signature4],
-  //     ] = await Promise.all(
-  //       [0, 1, 2, 3, 4].map(async (i) => {
-  //         // Signing environment
-  //         const wallet = await Secp256k1HdWallet.fromMnemonic(faucet.mnemonic, {
-  //           hdPaths: [makeLinkPath(i)],
-  //         });
-  //         const pubkey = encodeSecp256k1Pubkey((await wallet.getAccounts())[0].pubkey);
-  //         const address = (await wallet.getAccounts())[0].address;
-  //         const signingClient = await SigningStargateClient.offline(wallet);
-  //         const signerData: SignerData = {
-  //           sequence: signingInstruction.sequence,
-  //           chainId: signingInstruction.chainId,
-  //         };
-  //         const { bodyBytes: bb, signatures } = await signingClient.sign(
-  //           address,
-  //           signingInstruction.msgs,
-  //           signingInstruction.fee,
-  //           signingInstruction.memo,
-  //           signingInstruction.sigBlockHeight,
-  //           signerData,
-  //         );
-  //         return [pubkey, signatures[0], bb] as const;
-  //       }),
-  //     );
-  //
-  //     // From here on, no private keys are required anymore. Any anonymous entity
-  //     // can collect, assemble and broadcast.
-  //     {
-  //       const multisigPubkey = createMultisigThresholdPubkey(
-  //         [pubkey0, pubkey1, pubkey2, pubkey3, pubkey4],
-  //         2,
-  //       );
-  //       console.log("multisigAddress:", pubkeyToAddress(multisigPubkey, "link"));
-  //       expect(pubkeyToAddress(multisigPubkey, "link")).toEqual(multisigAccountAddress);
-  //
-  //       const address0 = pubkeyToAddress(pubkey0, "link");
-  //       const address1 = pubkeyToAddress(pubkey1, "link");
-  //       const address2 = pubkeyToAddress(pubkey2, "link");
-  //       const address3 = pubkeyToAddress(pubkey3, "link");
-  //       const address4 = pubkeyToAddress(pubkey4, "link");
-  //       console.log("add0:", address0);
-  //       console.log("add1:", address1);
-  //       console.log("add2:", address2);
-  //       console.log("add3:", address3);
-  //       console.log("add4:", address4);
-  //
-  //       const broadcaster = await StargateClient.connect(simapp.tendermintUrl);
-  //       const signedTx = makeMultisignedTx(
-  //         multisigPubkey,
-  //         signingInstruction.sequence,
-  //         signingInstruction.fee,
-  //         bodyBytes,
-  //         new Map<string, Uint8Array>([
-  //           [address0, signature0],
-  //           [address1, signature1],
-  //           [address2, signature2],
-  //           [address3, signature3],
-  //           [address4, signature4],
-  //         ]),
-  //         signingInstruction.sigBlockHeight,
-  //       );
-  //       // ensure signature is valid
-  //       const result = await broadcaster.broadcastTx(Uint8Array.from(TxRaw.encode(signedTx).finish()));
-  //       assertIsDeliverTxSuccess(result);
-  //     }
-  //   });
-  // });
+  describe("makeMultisignedTx", () => {
+    it("works", async () => {
+      pendingWithoutSimapp();
+      // const multisigAccountAddress = "cosmos1h90ml36rcu7yegwduzgzderj2jmq49hcpfclw9";
+      const multisigAccountAddress = "link15l2sszad8s390zpshtas030j48xav6nt9kp3dl";
+  
+      // On the composer's machine signing instructions are created.
+      // The composer does not need to be one of the signers.
+      const signingInstruction = await (async () => {
+        const client = await StargateClient.connect(simapp.tendermintUrl);
+        const accountOnChain = await client.getAccount(multisigAccountAddress);
+        const latestBlockHeight = await client.getHeight();
+        assert(accountOnChain, "Account does not exist on chain");
+  
+        const msgSend: MsgSend = {
+          fromAddress: multisigAccountAddress,
+          toAddress: "link1g7gsgktl9yjqatacswlwvns5yzy4u5jehsx2pz",
+          amount: coins(1234, "cony"),
+        };
+        const msg: MsgSendEncodeObject = {
+          typeUrl: "/lbm.bank.v1.MsgSend",
+          value: msgSend,
+        };
+        const gasLimit = 200000;
+        const fee = {
+          amount: coins(2000, "cony"),
+          gas: gasLimit.toString(),
+        };
+  
+        return {
+          accountNumber: accountOnChain.accountNumber,
+          sequence: accountOnChain.sequence,
+          chainId: await client.getChainId(),
+          msgs: [msg],
+          fee: fee,
+          sigBlockHeight: latestBlockHeight,
+          memo: "Use your tokens wisely",
+        };
+      })();
+  
+      const [
+        [pubkey0, signature0, bodyBytes],
+        [pubkey1, signature1],
+        [pubkey2, signature2],
+        [pubkey3, signature3],
+        [pubkey4, signature4],
+      ] = await Promise.all(
+        [0, 1, 2, 3, 4].map(async (i) => {
+          // Signing environment
+          const wallet = await Secp256k1HdWallet.fromMnemonic(faucet.mnemonic, {
+            hdPaths: [makeLinkPath(i)],
+          });
+          const pubkey = encodeSecp256k1Pubkey((await wallet.getAccounts())[0].pubkey);
+          const address = (await wallet.getAccounts())[0].address;
+          const signingClient = await SigningStargateClient.offline(wallet);
+          const signerData: SignerData = {
+            accountNumber: signingInstruction.accountNumber,
+            sequence: signingInstruction.sequence,
+            chainId: signingInstruction.chainId,
+          };
+          const { bodyBytes: bb, signatures } = await signingClient.sign(
+            address,
+            signingInstruction.msgs,
+            signingInstruction.fee,
+            signingInstruction.memo,
+            // signingInstruction.sigBlockHeight,
+            signerData,
+          );
+          return [pubkey, signatures[0], bb] as const;
+        }),
+      );
+  
+      // From here on, no private keys are required anymore. Any anonymous entity
+      // can collect, assemble and broadcast.
+      {
+        const multisigPubkey = createMultisigThresholdPubkey(
+          [pubkey0, pubkey1, pubkey2, pubkey3, pubkey4],
+          2,
+        );
+        expect(pubkeyToAddress(multisigPubkey, "link")).toEqual(multisigAccountAddress);
+  
+        const address0 = pubkeyToAddress(pubkey0, "link");
+        const address1 = pubkeyToAddress(pubkey1, "link");
+        const address2 = pubkeyToAddress(pubkey2, "link");
+        const address3 = pubkeyToAddress(pubkey3, "link");
+        const address4 = pubkeyToAddress(pubkey4, "link");
+  
+        const broadcaster = await StargateClient.connect(simapp.tendermintUrl);
+        const signedTx = makeMultisignedTx(
+          multisigPubkey,
+          signingInstruction.sequence,
+          signingInstruction.fee,
+          bodyBytes,
+          new Map<string, Uint8Array>([
+            [address0, signature0],
+            [address1, signature1],
+            [address2, signature2],
+            [address3, signature3],
+            [address4, signature4],
+          ]),
+          // signingInstruction.sigBlockHeight,
+        );
+        // ensure signature is valid
+        const result = await broadcaster.broadcastTx(Uint8Array.from(TxRaw.encode(signedTx).finish()));
+        assertIsDeliverTxSuccess(result);
+      }
+    });
+  });
 });
