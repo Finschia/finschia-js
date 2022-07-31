2487b209e971ad7839f1053ab0c2abefc35f0a54
zemyblue <zemyblue@gmail.com>
2022-07-31 14:59:46 +0900

    chore: delete unused ostracon module in stargate.
    Signed-off-by: zemyblue <zemyblue@gmail.com>

--- packages/stargate/src/ostracon/requests.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-
-export interface BlockSearchParams {
-  readonly query: string;
-  readonly page?: number;
-  readonly per_page?: number;
-  readonly order_by?: string;
-}
--- packages/stargate/src/ostracon/responses.ts
+++
@@ -1,6 +0,0 @@
-import { BlockResponse } from "@cosmjs/tendermint-rpc";
-
-export interface BlockSearchResponse {
-  readonly blocks: readonly BlockResponse[];
-  readonly totalCount: number;
-}
