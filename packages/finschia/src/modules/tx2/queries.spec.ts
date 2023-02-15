fed1c2cabbdc3167cee5538fd828ba27192f42e6
zemyblue <zemyblue@gmail.com>
2023-02-15 19:30:22 +0900

    fix: local test error of `Tx2Extension`
    Signed-off-by: zemyblue <zemyblue@gmail.com>

--- packages/finschia/src/modules/tx2/queries.spec.ts
+++ packages/finschia/src/modules/tx2/queries.spec.ts
@@ -1,8 +1,8 @@
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { coins, MsgSendEncodeObject, QueryClient } from "@cosmjs/stargate";
 import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
+import { assert } from "@cosmjs/utils";
 import { MsgSend } from "cosmjs-types/cosmos/bank/v1beta1/tx";
-import { TxRaw } from "cosmjs-types/cosmos/tx/v1beta1/tx";
 
 import { makeLinkPath } from "../../paths";
 import { SigningFinschiaClient } from "../../signingfinschiaclient";
@@ -12,6 +12,7 @@ import {
   makeRandomAddress,
   pendingWithoutSimapp,
   simapp,
+  simappEnabled,
 } from "../../testutils.spec";
 import { longify } from "../../utils";
 import { setupTx2Extension, Tx2Extension } from "./queries";
@@ -21,53 +22,50 @@ async function makeClientWithTx2(rpcUrl: string): Promise<[QueryClient & Tx2Exte
   return [QueryClient.withExtensions(tmClient, setupTx2Extension), tmClient];
 }
 
-async function sendDummyTx() {
-  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(faucet.mnemonic, {
-    hdPaths: [makeLinkPath(0)],
-    prefix: simapp.prefix,
-  });
-  const client = await SigningFinschiaClient.connectWithSigner(
-    simapp.tendermintUrl,
-    wallet,
-    defaultSigningClientOptions,
-  );
-  const defaultFee = {
-    amount: coins(250000, simapp.denomFee),
-    gas: "1500000",
-  };
-  const msg: MsgSend = {
-    fromAddress: faucet.address0,
-    toAddress: makeRandomAddress(),
-    amount: coins(1234, simapp.denomFee),
-  };
-  const dummyMsg: MsgSendEncodeObject = {
-    typeUrl: "/cosmos.bank.v1beta1.MsgSend",
-    value: msg,
-  };
-  const signed = await client.sign(faucet.address0, [dummyMsg], defaultFee, "");
-  return await client.broadcastTx(Uint8Array.from(TxRaw.encode(signed).finish()));
-}
-
 describe("Tx2Extension", () => {
-  let client: QueryClient & Tx2Extension;
-  let tmClient: Tendermint34Client;
+  let txHeight: number | undefined;
+
   beforeAll(async () => {
-    const res = await makeClientWithTx2(simapp.tendermintUrl);
-    client = res[0];
-    tmClient = res[1];
-  });
-  afterAll(() => {
-    tmClient.disconnect();
+    if (simappEnabled()) {
+      const wallet = await DirectSecp256k1HdWallet.fromMnemonic(faucet.mnemonic, {
+        hdPaths: [makeLinkPath(0)],
+        prefix: simapp.prefix,
+      });
+      const client = await SigningFinschiaClient.connectWithSigner(
+        simapp.tendermintUrl,
+        wallet,
+        defaultSigningClientOptions,
+      );
+      const defaultFee = {
+        amount: coins(250000, simapp.denomFee),
+        gas: "1500000",
+      };
+      const msg: MsgSend = {
+        fromAddress: faucet.address0,
+        toAddress: makeRandomAddress(),
+        amount: coins(1234, simapp.denomFee),
+      };
+      const dummyMsg: MsgSendEncodeObject = {
+        typeUrl: "/cosmos.bank.v1beta1.MsgSend",
+        value: msg,
+      };
+      const result = await client.signAndBroadcast(faucet.address0, [dummyMsg], defaultFee);
+      txHeight = result.height;
+
+      client.disconnect();
+    }
   });
 
   it("getBlockWithTxs", async () => {
     pendingWithoutSimapp();
+    assert(txHeight, "Missing txHeight");
 
-    const txRes = await sendDummyTx();
-    const response = await client.tx2.getBlockWithTxs(longify(txRes.height));
+    const [client, tmClient] = await makeClientWithTx2(simapp.tendermintUrl);
+    const response = await client.tx2.getBlockWithTxs(longify(txHeight));
 
     expect(response.txs).toBeDefined();
     expect(response.blockId).toBeDefined();
     expect(response.block).toBeDefined();
+    tmClient.disconnect();
   });
 });
