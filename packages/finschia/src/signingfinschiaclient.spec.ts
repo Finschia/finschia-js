1b49ad818e909ae07c8a8f97a521075a35c79ea5
jaeseung.bae <jaeseung.bae@linecorp.com>
2023-02-23 22:30:29 +0900

    feat: add MsgInstantiateContract2

--- packages/finschia/src/signingfinschiaclient.spec.ts
+++ packages/finschia/src/signingfinschiaclient.spec.ts
@@ -51,6 +51,7 @@ import {
   unused,
   validator,
 } from "./testutils.spec";
+import { instantiate2Address } from "./modules/wasm/util";
 
 describe("SigningFinschiaClient", () => {
   describe("connectWithSigner", () => {
@@ -461,6 +462,55 @@ describe("SigningFinschiaClient", () => {
         defaultInstantiateFee,
       );
 
+      client.disconnect();
+    });
+  });
+
+  describe("instantiate2", () => {
+    it("can instantiate with predictable address", async () => {
+      // Arrange
+      pendingWithoutSimapp();
+      const wallet = await DirectSecp256k1HdWallet.fromMnemonic(faucet.mnemonic, {
+        hdPaths: [makeLinkPath(0)],
+        prefix: simapp.prefix,
+      });
+      const options = { ...defaultSigningClientOptions, prefix: simapp.prefix };
+
+      const client = await SigningFinschiaClient.connectWithSigner(simapp.tendermintUrl, wallet, options);
+      const { codeId } = await client.upload(faucet.address0, getHackatom().data, defaultUploadFee);
+      const funds = [coin(1234, "cony"), coin(321, "stake")];
+      const beneficiaryAddress = makeRandomAddress();
+      let salt = Uint8Array.from([0x01]);
+      const wasm = getHackatom().data;
+      const msg = {
+        verifier: faucet.address0,
+        beneficiary: beneficiaryAddress,
+      };
+      const expectedAddress = instantiate2Address(
+        sha256(wasm),
+        faucet.address0,
+        salt,
+        JSON.stringify(msg),
+        simapp.prefix,
+      );
+
+      // Act
+      const { contractAddress } = await client.instantiate2(
+        faucet.address0,
+        codeId,
+        msg,
+        "My cool label--",
+        defaultInstantiateFee,
+        {
+          memo: "Let's see if the memo is used",
+          funds: funds,
+          salt: salt,
+          fixMsg: true,
+        },
+      );
+
+      // Assert
+      expect(contractAddress).toEqual(expectedAddress);
       client.disconnect();
     });
   });
